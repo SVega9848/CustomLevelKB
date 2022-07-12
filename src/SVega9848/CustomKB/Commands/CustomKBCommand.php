e30581448f5bb1a28a0c5fc19c54c0ca1f7a1d9b
SVega9848 <108686235+SVega9848@users.noreply.github.com>
2022-07-12 12:51:38 +0200

    Add files via upload

---
+++ src/SVega9848/CustomKB/Commands/CustomKBCommand.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace SVega9848\CustomKB\Commands;
+
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\command\utils\CommandException;
+use pocketmine\lang\Translatable;
+use pocketmine\player\Player;
+use pocketmine\utils\Config;
+use pocketmine\utils\TextFormat;
+use SVega9848\CustomKB\Core\Main;
+
+class CustomKBCommand extends Command {
+
+    private $main;
+
+    public function __construct(Main $main)
+    {
+        parent::__construct("customkb", "Modify your servers kb!", "", ["ckb"]);
+        $this->main = $main;
+    }
+
+    public function execute(CommandSender $sender, string $commandLabel, array $args)
+    {
+        if($sender->hasPermission("customkb.cmd")) {
+            if($sender instanceof Player) {
+
+                $config = new Config($this->main->getDataFolder(). "/". $sender->getWorld()->getFolderName(). ".yml", Config::YAML);
+
+                if(isset($args[0])) {
+                    if(isset($args[1])) {
+                        switch($args[0]) {
+                            case "setkb":
+
+                                $config->set("world", $sender->getWorld()->getFolderName());
+                                $config->set("kb", $args[1]);
+                                $config->save();
+                                $sender->sendMessage(TextFormat::GREEN. "You succesfully changed the kb to ". TextFormat::DARK_GREEN. $args[1]. TextFormat::GREEN. " in the world ". TextFormat::DARK_GREEN. $sender->getWorld()->getFolderName());
+
+                                break;
+                            case "setdelay":
+
+                                $config->set("world", $sender->getWorld()->getFolderName());
+                                $config->set("delay", $args[1]);
+                                $config->save();
+                                $sender->sendMessage(TextFormat::GREEN. "You succesfully changed the delay hit to ". TextFormat::DARK_GREEN. $args[1]. TextFormat::GREEN. " in the world ". TextFormat::DARK_GREEN. $sender->getWorld()->getFolderName());
+
+                                break;
+                            default:
+                                $sender->sendMessage(TextFormat::RED. "Missing arguments. Try /ckb (setkb/setdelay) (amount)");
+                                break;
+                    }
+                } else {
+                    $sender->sendMessage(TextFormat::RED. "Missing arguments. Use /ckb (setkb/setdelay) (amount)");
+                }
+            }} else {
+                $sender->sendMessage(TextFormat::RED. "Missing arguments. Use /ckb (setkb/setdelay)");
+            }
+        } else {
+            $sender->sendMessage(TextFormat::RED. "You do not have permissions to use this command");
+        }
+        return true;
+    }
+}
---
+++ src/SVega9848/CustomKB/Core/Main.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace SVega9848\CustomKB\Core;
+
+use SVega9848\CustomKB\Commands\CustomKBCommand;
+use pocketmine\event\Listener;
+use pocketmine\event\server\CommandEvent;
+use pocketmine\plugin\PluginBase;
+use pocketmine\scheduler\CancelTaskException;
+use pocketmine\scheduler\ClosureTask;
+use SVega9848\CustomKB\Listener\Events;
+
+class Main extends PluginBase {
+
+	public function onEnable() : void{
+		$this->getServer()->getPluginManager()->registerEvents(new Events($this), $this);
+        $this->getServer()->getCommandMap()->register("customkb", new CustomKBCommand($this));
+	}
+
+    
+
+}
